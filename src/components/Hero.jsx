6c85513291e15d49eca8a4a18404311c3d0290a1
dan <md.erhenede@gmail.com>
2024-05-09 10:17:42 +0100

    pp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import resume from '../assets/resume.pdf'
+import resume from '../assets/Resume.pdf'
 import {Cursor, useTypewriter} from 'react-simple-typewriter'
 
 
@@ -20,7 +19,7 @@ function Hero() {
               </span>
             </h1>
             <p className='text-sm md:text-base lg:text-sm xl:text-sm py-4'>
-            As a multi-disciplinary professional in Front End Development, Graphic Design, UI/UX, and 3D Art, my skills cover coding, visual design, and user experience, making me a versatile asset. I excel at crafting user-friendly websites, creating compelling visuals, and designing intuitive interfaces. My ability to blend creativity with functionality makes me an ideal choice to enhance any team's digital presence.
+            As a multi-disciplinary professional in Front End Development, Graphic Design, UI/UX, and 3D Art, my skills cover coding, visual design, and user experience, making me a versatile asset. I excel at crafting user-friendly websites, creating compelling visuals, and designing intuitive interfaces. My ability to blend creativity with functionality makes me an ideal choice to enhance any teams digital presence.
             </p>
             <div className='flex flex-col gap-2 md:flex-row md:gap-4'>
               <a href={resume} className='w-full md:w-[30vh] bg-[#A98F76] py-2 md:py-3 text-[#242948] text-center'>
